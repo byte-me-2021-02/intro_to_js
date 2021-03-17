5b5fc5904e3c1da6a509a927cb605968849359cb
brian <riceb53@gmail.com>
2021-03-16 21:48:06 -0400

    add loops

---
+++ loops.js
@@ -0,0 +1,25 @@
+var fruits = ["palmelo", "golden nugget mandarin", "pineapple", "mango"];
+
+// while
+var i = 0;
+
+while (i < fruits.length) {
+  console.log(fruits[i]);
+  // i = i + 1;
+  // i += 1;
+  i++;
+}
+
+
+// fruits.each do |fruit|
+
+// fruits.forEach(function(fruit) {
+//   console.log(fruit);
+// });
+
+
+// for loop
+
+// for (var i = 0; i < fruits.length; i += 1) {
+//   console.log(fruits[i]);
+// }
